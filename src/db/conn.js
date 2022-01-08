020cb7255757194557efef2a974531b9d5c67119
gautam <gautamsingh7424@gmail.com>
2022-01-08 22:09:42 +0530

    frontend completion successfull

---
+++ src/db/conn.js
@@ -0,0 +1,7 @@
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/youtubeRegistration").then(()=>{
+    console.log("Connection Successfull");
+}).catch((e)=>{
+    console.log("no connection");
+})
---
+++ src/models/registers.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+
+const employeeSchema=new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        
+    }
+})
+
+//now we need to create collections
+
+const Register=new mongoose.model("Register",employeeSchema);
+module.exports=Register;
